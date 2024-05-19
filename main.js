ada0343aac180e28124101acdedefa3a38844ad1
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-05-19 16:08:36 +0700

    Basic UI

---
+++ main.js
@@ -0,0 +1,5 @@
+import Todo from "./src/js/Todo.js";
+import "./style.css";
+
+const todo = new Todo();
+todo.init();
---
+++ src/js/Todo.js
@@ -0,0 +1,7 @@
+class Todo {
+  init() {
+    console.log("Todo List App start");
+  }
+}
+
+export default Todo;
